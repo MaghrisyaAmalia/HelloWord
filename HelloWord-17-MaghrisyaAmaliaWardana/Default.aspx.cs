96e1880f020973023dd745ded5efde65c4b65522
Maghrisya Amalia Wardana <aamaliaw209@gmail.com>
2017-02-12 19:46:52 +0700

    Add project files.

---
+++ HelloWord-17-MaghrisyaAmaliaWardana/Default.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace HelloWord_17_MaghrisyaAmaliaWardana
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
